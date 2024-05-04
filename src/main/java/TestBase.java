fe8365b2b9de5cced1d8b2db5c4a32938eafbf82
Kayaththiri <thillaikayatk@gmail.com>
2024-05-04 16:50:10 +0200

    record video using context

--- src/main/java/TestBase.java
+++ src/main/java/TestBase.java
@@ -1,20 +1,23 @@
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+import java.nio.file.Paths;
+
 
 public class TestBase {
     Playwright playwright;
     Browser browser;
     Page page;
+    BrowserContext context;
 
     @BeforeTest
     public void setUp(){
          playwright = Playwright.create();
          browser= playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-         page = browser.newPage();
+         //page = browser.newPage();
+        context=browser.newContext(new Browser.NewContextOptions().setRecordVideoDir(Paths.get("videos/record")));
+        page=context.newPage();
         page.navigate("https://opensource-demo.orangehrmlive.com/");
     }
 
