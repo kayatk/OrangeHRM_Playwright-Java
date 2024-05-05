06f78f4340f578eb8c85f101e9614c2c69529ba6
Kayaththiri <thillaikayatk@gmail.com>
2024-05-05 09:43:51 +0200

    maximize screen

--- src/main/java/TestBase.java
+++ src/main/java/TestBase.java
@@ -3,6 +3,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 import java.nio.file.Paths;
+import java.util.List;
 
 
 public class TestBase {
@@ -12,19 +13,42 @@ public class TestBase {
     BrowserContext context;
 
     @BeforeTest
-    public void setUp(){
-         playwright = Playwright.create();
+    public void setUp() throws InterruptedException{
+        /* playwright = Playwright.create();
          browser= playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-         //page = browser.newPage();
+
+         //Approach #1
+         page = browser.newPage();*/
+
+        //Approach#2
+        /*
         context=browser.newContext(new Browser.NewContextOptions().setRecordVideoDir(Paths.get("videos/record")));
-        page=context.newPage();
-        page.navigate("https://opensource-demo.orangehrmlive.com/");
+        page=context.newPage();*/
+
+        //Approach #3
+      /*  context=browser.newContext(new Browser.NewContextOptions().setViewportSize(1600,990));
+        page=context.newPage();*/
+       // page.navigate("https://opensource-demo.orangehrmlive.com/");
+
+        //Approach #4
+
+        browser= Playwright.create().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)
+                .setArgs(List.of("--start-maximized")));
+        context=browser.newContext(new Browser.NewContextOptions().setViewportSize(null));
+        page= context.newPage();
+
+       /* page = Playwright.create().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)
+                .setArgs(List.of("--start-maximized"))).newContext(new Browser.NewContextOptions().setViewportSize(null)).newPage();*/
+
+
+
+
     }
 
     @AfterTest
     public void tearDown(){
         page.close();
         browser.close();
-        playwright.close();
+      //  playwright.close();
     }
 }
--- src/main/java/plus.java
+++ src/main/java/plus.java
@@ -14,24 +14,24 @@ public class plus extends TestBase {
 
     @Test
     public void launchBrowser(){
-        //String actualTitle =page.title();
-        //String expectedTitle ="OrangeHRM";
-        // assertEquals(actualTitle,expectedTitle);
-//        assertThat(page).hasTitle("OrangeHRM");
-//        page.getByPlaceholder("Username").fill("Admin");
-//        page.getByPlaceholder("Password").fill("admin123");
-//        page.click("[type=\"submit\"]");
-//        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("screenshot/Dashboard.png")));
-//        Locator dashboardTitle = page.locator("//h6[text()='Dashboard']");
-//        dashboardTitle.isVisible();
-//        assertThat(page.locator("//p[@class='oxd-userdropdown-name']")).hasText("Megha Phophalia sawalkar");
-//        page.navigate("https://www.lambdatest.com/selenium-playground/checkbox-demo");
-//        Locator chkbox =page.locator("//input[@id ='isAgeSelected']");
-//        assertThat(chkbox).not().isChecked();
-//        chkbox.check();
-//        assertThat(chkbox).isChecked();
+    /*    String actualTitle =page.title();
+        String expectedTitle ="OrangeHRM";
+         assertEquals(actualTitle,expectedTitle);
+        assertThat(page).hasTitle("OrangeHRM");
+        page.getByPlaceholder("Username").fill("Admin");
+        page.getByPlaceholder("Password").fill("admin123");
+        page.click("[type=\"submit\"]");
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("screenshot/Dashboard.png")));
+        Locator dashboardTitle = page.locator("//h6[text()='Dashboard']");
+        dashboardTitle.isVisible();
+        assertThat(page.locator("//p[@class='oxd-userdropdown-name']")).hasText("Megha Phophalia sawalkar");
+        page.navigate("https://www.lambdatest.com/selenium-playground/checkbox-demo");
+        Locator chkbox =page.locator("//input[@id ='isAgeSelected']");
+        assertThat(chkbox).not().isChecked();
+        chkbox.check();
+        assertThat(chkbox).isChecked();
 
-        //DROP DOWN
+        //-------------------------------------------------------------------DROP DOWN -------------------------------------------------------------------
 
         page.navigate("https://www.lambdatest.com/selenium-playground/select-dropdown-demo");
         //Single option
@@ -66,10 +66,18 @@ public class plus extends TestBase {
         grpDrpdwn.selectOption("Ruby");
         page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("screenshot/Ruby.png")).setFullPage(true));
 
-        //Screenshot
+        //----------------------------------Screenshot--------------------------------------------------------------------------------
         //visible region
         page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("screenshot/visibleRegion.png")));
         page.screenshot(new Page.ScreenshotOptions().setFullPage(true).setPath(Paths.get("screenshot/FullPage.png")));
         page.screenshot(new Page.ScreenshotOptions().setMask(Arrays.asList(grpDrpdwn)).setPath(Paths.get("screenshot/Masking.png")));
+*/
+        //---------------------------------------------WINDOW HANDLINGS----------------------------------------------------------------
+
+        page.navigate("https://www.lambdatest.com/selenium-playground/window-popup-modal-demo");
+        Locator tab=page.getByText("Follow On Twitter");
+        tab.click();
+
+
     }
 }
